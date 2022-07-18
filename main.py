a2ee5d1ac2267bcd6faa4bf430fbd7cb8bdb264d
대현 <rkdeown10@naver.com>
2022-07-18 11:58:21 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+﻿import requests
+from bs4 import BeautifulSoup as bs
+
+status = ""
+user = "daehyuh318" //your username
+repo = "daehyuh318" //your repo
+fullurl = user + "%2F" + repo
+
+while True:
+    try:
+        page = requests.get(
+            "https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2F" + fullurl)
+        soup = bs(page.text, "html.parser")
+        pry = soup.find_all("text")[3].string
+        if status != pry:
+          print(pry)
+          status = pry
+    except:
+        pass
